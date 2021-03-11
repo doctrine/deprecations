269e9a916ebdcaa99456c5881045b1d206d2e84b
Benjamin Eberlei <kontakt@beberlei.de>
2021-03-11 20:07:57 +0100

    [GH-17] Allow verify deprecation was not triggered in test.

--- lib/Doctrine/Deprecations/PHPUnit/VerifyDeprecations.php
+++ lib/Doctrine/Deprecations/PHPUnit/VerifyDeprecations.php
@@ -13,9 +13,17 @@ trait VerifyDeprecations
     /** @var array<string,int> */
     private $doctrineDeprecationsExpectations = [];
 
+    /** @var array<string,int> */
+    private $doctrineNoDeprecationsExpectations = [];
+
     public function expectDeprecationWithIdentifier(string $identifier): void
     {
         $this->doctrineDeprecationsExpectations[$identifier] = Deprecation::getTriggeredDeprecations()[$identifier] ?? 0;
+    }
+
+    public function expectNoDeprecationWithIdentifier(string $identifier): void
+    {
+        $this->doctrineNoDeprecationsExpectations[$identifier] = Deprecation::getTriggeredDeprecations()[$identifier] ?? 0;
     }
 
     /**
@@ -30,6 +38,18 @@ trait VerifyDeprecations
                 $actualCount > $expectation,
                 sprintf(
                     "Expected deprecation with identifier '%s' was not triggered by code executed in test.",
+                    $identifier
+                )
+            );
+        }
+
+        foreach ($this->doctrineNoDeprecationsExpectations as $identifier => $expectation) {
+            $actualCount = Deprecation::getTriggeredDeprecations()[$identifier] ?? 0;
+
+            $this->assertTrue(
+                $actualCount === $expectation,
+                sprintf(
+                    "Expected deprecation with identifier '%s' was triggered by code executed in test, but expected not to.",
                     $identifier
                 )
             );
